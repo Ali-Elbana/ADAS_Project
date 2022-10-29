0ffdb834d2b2d35ccbb55ea1b333a25ec36c6d1e
Mohamed Alaa <abo.3ala2.a338@gmail.com>
2022-10-29 19:35:31 +0200

    Remove Relase & debug folders

--- Debug/system/src/cmsis/vectors_stm32f401xc.d
+++
@@ -1,5 +0,0 @@
-system/src/cmsis/vectors_stm32f401xc.o: \
- ../system/src/cmsis/vectors_stm32f401xc.c \
- ../system/include/cortexm/ExceptionHandlers.h
-
-../system/include/cortexm/ExceptionHandlers.h:
