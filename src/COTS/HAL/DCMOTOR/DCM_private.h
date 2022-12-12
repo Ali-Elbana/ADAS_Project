4be421a16081cf39d95f443a11003a1e80c4d7b3
Mohamed Alaa <abo.3ala2.a338@gmail.com>
2022-12-12 20:31:38 +0200

    Moved the motor configuration to inferface.h

--- src/COTS/HAL/DCMOTOR/DCM_private.h
+++ src/COTS/HAL/DCMOTOR/DCM_private.h
@@ -10,32 +10,4 @@
 #ifndef _DCM_private_H
 #define _DCM_private_H
 
-/**
- * @struct DCM_MotorConfiguration
- * @brief DC Motor configuration structure for motor initialization
- * DC Motor configuration for motor initialization for a certain port and pin
- * Since DC Motors have 2 sides to be controller, we will intiliaze each motor with 2 pins.
- * Taking into consideration that the pins could be on different ports
- */
-
-typedef struct DCM_private
-{
-    /**
-     * @brief Initialize the DC Motor on a certain port
-     */
-    u8_t Side1Port;
-    /**
-     * @brief Initialize the DC Motor on a certain pin
-     */
-    u8_t Side1Pin;
-    /**
-     * @brief Initialize the DC Motor on a certain port
-     */
-    u8_t Side2Port;
-    /**
-     * @brief Initialize the DC Motor on a certain pin
-     */
-    u8_t Side2Pin;
-} DCM_MotorConfiguration;
-
 #endif //_DCM_private_H
