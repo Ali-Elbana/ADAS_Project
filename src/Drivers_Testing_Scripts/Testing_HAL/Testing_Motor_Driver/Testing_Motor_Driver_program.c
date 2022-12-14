1cfb81b5ac24d4042daba370a569fa697b342366
Mohamed Alaa <abo.3ala2.a338@gmail.com>
2022-12-14 15:40:03 +0200

    Updated testing application

--- src/Drivers_Testing_Scripts/Testing_HAL/Testing_Motor_Driver/Testing_Motor_Driver_program.c
+++ src/Drivers_Testing_Scripts/Testing_HAL/Testing_Motor_Driver/Testing_Motor_Driver_program.c
@@ -42,53 +42,62 @@ void THDC_vTestMotors(void)
         .u8Port			= GPIO_PORTA,
         .u8Pin1 		= GPIOx_PIN0,
         .u8Pin2 		= GPIOx_PIN1,
-        .u8Direction 	= FORWARD
+        .u8Direction 	= INITIAL_ZERO
     };
 
     // Initialize motor 2
-    VAR(DCM_MotorConfiguration) GS_pMotor2 =
-    {
-        .u8Port 		= GPIO_PORTA,
-        .u8Pin1 		= GPIOx_PIN2,
-        .u8Pin2 		= GPIOx_PIN3,
-        .u8Direction 	= FORWARD
-    };
+    // VAR(DCM_MotorConfiguration) GS_pMotor2 =
+    // {
+    //     .u8Port 		= GPIO_PORTA,
+    //     .u8Pin1 		= GPIOx_PIN2,
+    //     .u8Pin2 		= GPIOx_PIN3,
+    //     .u8Direction 	= FORWARD
+    // };
 
-    // Initialize motor 3
-    VAR(DCM_MotorConfiguration) GS_pMotor3 =
-    {
-        .u8Port 		= GPIO_PORTA,
-        .u8Pin1 		= GPIOx_PIN8,
-        .u8Pin2 		= GPIOx_PIN9,
-        .u8Direction 	= BACKWARD
-    };
+    // // Initialize motor 3
+    // VAR(DCM_MotorConfiguration) GS_pMotor3 =
+    // {
+    //     .u8Port 		= GPIO_PORTA,
+    //     .u8Pin1 		= GPIOx_PIN8,
+    //     .u8Pin2 		= GPIOx_PIN9,
+    //     .u8Direction 	= BACKWARD
+    // };
 
-    // Initialize motor 4
-    VAR(DCM_MotorConfiguration) GS_pMotor4 =
-    {
-        .u8Port 		= GPIO_PORTA ,
-        .u8Pin1 		= GPIOx_PIN10,
-        .u8Pin2 		= GPIOx_PIN11,
-        .u8Direction 	= BACKWARD
-    };
+    // // Initialize motor 4
+    // VAR(DCM_MotorConfiguration) GS_pMotor4 =
+    // {
+    //     .u8Port 		= GPIO_PORTA ,
+    //     .u8Pin1 		= GPIOx_PIN10,
+    //     .u8Pin2 		= GPIOx_PIN11,
+    //     .u8Direction 	= BACKWARD
+    // };
 
     HDCM_vInitMotor(&GS_pMotor1);
-    HDCM_vInitMotor(&GS_pMotor2);
-    HDCM_vInitMotor(&GS_pMotor3);
-    HDCM_vInitMotor(&GS_pMotor4);
+    // HDCM_vInitMotor(&GS_pMotor2);
+    // HDCM_vInitMotor(&GS_pMotor3);
+    // HDCM_vInitMotor(&GS_pMotor4);
 
     while (TRUE)
     {
 
-        for (u32DelayCounter = 0; u32DelayCounter < 5000000; u32DelayCounter++) ;
+        for (u32DelayCounter = 0; u32DelayCounter < 10000000; u32DelayCounter++) ;
+
+        HDCM_vMoveForward(&GS_pMotor1);
+        // HDCM_vMoveBackward(&GS_pMotor1);
+        // HDCM_vMoveBackward(&GS_pMotor2);
+        // HDCM_vMoveForward (&GS_pMotor3);
+        // HDCM_vMoveForward (&GS_pMotor4);
+
+        for (u32DelayCounter = 0; u32DelayCounter < 10000000; u32DelayCounter++) ;
+
+        HDCM_vStopMotor(&GS_pMotor1);
+        // HDCM_vStopMotor(&GS_pMotor2);
+        // HDCM_vStopMotor(&GS_pMotor3);
+        // HDCM_vStopMotor(&GS_pMotor4);
+
+        for (u32DelayCounter = 0; u32DelayCounter < 10000000; u32DelayCounter++) ;
 
         HDCM_vMoveBackward(&GS_pMotor1);
-        HDCM_vMoveBackward(&GS_pMotor2);
-        HDCM_vMoveForward (&GS_pMotor3);
-        HDCM_vMoveForward (&GS_pMotor4);
-
-        for (u32DelayCounter = 0; u32DelayCounter < 5000000; u32DelayCounter++) ;
-
     }
 
 
