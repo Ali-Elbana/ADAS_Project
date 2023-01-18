3101f10814276d6521d78109222b721696f006a0
Mohamed Alaa <abo.3ala2.a338@gmail.com>
2023-01-18 19:08:18 +0200

    Added testing function

--- src/Drivers_Testing_Scripts/Testing_Systems/Testing_ACC_Demo/Testing_ACC_Demo_interface.h
+++ src/Drivers_Testing_Scripts/Testing_Systems/Testing_ACC_Demo/Testing_ACC_Demo_interface.h
@@ -1,4 +1,10 @@
 #ifndef _TESTING_ACC_DEMO_INTERFACE_H
 #define _TESTING_ACC_DEMO_INTERFACE_H
 
+/************************************************************************/
+/*                        Functions prototypes                          */
+/************************************************************************/
+
+void TSACC_CarControlDemo(void);
+
 #endif //_TESTING_ACC_DEMO_INTERFACE_H
