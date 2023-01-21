f0ca7b00faea70058840fe0dda2016e1b93b5ffd
Mohamed Alaa <abo.3ala2.a338@gmail.com>
2023-01-21 14:59:40 +0200

    Added Bluetooth exchanging functions

--- src/COTS/HAL/Bluetooth/Bluetooth_interface.h
+++ src/COTS/HAL/Bluetooth/Bluetooth_interface.h
@@ -1,8 +1,8 @@
-/* FILENAME: Bluetooth_interface  
-*  Author:  Ali El Bana
-*  Version:  V1.0
-*  DATE:   Thu 01/05/2023
-*/
+/* FILENAME: Bluetooth_interface
+ *  Author:  Ali El Bana
+ *  Version:  V1.0
+ *  DATE:   Thu 01/05/2023
+ */
 #ifndef _Bluetooth_interface_H
 #define _Bluetooth_interface_H
 
@@ -10,23 +10,23 @@
 /*                        Functions prototypes                          */
 /************************************************************************/
 
-void HBluetooth_vInit( void ) ;
+void HBluetooth_vInit(void);
 
-void HBluetooth_vSendByte( u8_t A_u8Byte ) ;
+void HBluetooth_vSendByte(VAR(u8_t) A_u8Byte);
 
-u8_t HBluetooth_u8ReceiveByte( void ) ;
+u8_t HBluetooth_u8ReceiveByte(void);
 
-void HBluetooth_vSendString( c8_t *A_ptrc8String ) ;
+void HBluetooth_vSendString(P2VAR(c8_t) A_ptrc8String);
 
-void HBluetooth_vReceiveString( c8_t A_c8YourString[] ) ;
+void HBluetooth_vReceiveString(P2VAR(c8_t) A_c8YourString);
 
-u8_t HBluetooth_u8CompStrings( c8_t *String1, c8_t *String2 ) ;
+u8_t HBluetooth_u8CompStrings(P2VAR(c8_t) String1, P2VAR(c8_t) String2);
 
-void HBluetooth_vEnable( void ) ;
+void HBluetooth_vEnable(void);
 
-void HBluetooth_vDisable( void ) ;
+void HBluetooth_vDisable(void);
 
-
-
+void HBluetooth_ExchangeString(P2VAR(c8_t) L_strMessagePlaceholder, P2VAR(c8_t) L_strInfo);
+u8_t HBluetooth_ExchangeByte(P2VAR(c8_t) L_strMessagePlaceholder);
 
 #endif //_Bluetooth_interface_H
--- src/COTS/HAL/Bluetooth/Bluetooth_program.c
+++ src/COTS/HAL/Bluetooth/Bluetooth_program.c
@@ -35,7 +35,7 @@ extern USART_MemoryMapType * 	Bluetooth_UART_ID 	;
 /*                     Functions' implementations                      	*/
 /************************************************************************/
 
-void HBluetooth_vInit( void )
+FUNC(void) HBluetooth_vInit( void )
 {
 
 	MUSART_vInit( &Bluetooth_Config, &Bluetooth_CLK, Bluetooth_UART_ID ) ;
@@ -45,7 +45,7 @@ void HBluetooth_vInit( void )
 /*******************************************************************************************************************/
 /******************************************************************************************************************/
 
-void HBluetooth_vSendByte( u8_t A_u8Byte )
+void HBluetooth_vSendByte(VAR(u8_t) A_u8Byte)
 {
 
 	MUSART_vTransmitByte( Bluetooth_UART_ID, A_u8Byte ) ;
@@ -55,7 +55,7 @@ void HBluetooth_vSendByte( u8_t A_u8Byte )
 /*******************************************************************************************************************/
 /******************************************************************************************************************/
 
-u8_t HBluetooth_u8ReceiveByte( void )
+FUNC(u8_t) HBluetooth_u8ReceiveByte( void )
 {
 
 	u8_t L_u8ReceivedByte = INITIAL_ZERO ;
@@ -69,7 +69,7 @@ u8_t HBluetooth_u8ReceiveByte( void )
 /*******************************************************************************************************************/
 /******************************************************************************************************************/
 
-void HBluetooth_vSendString( c8_t *A_ptrc8String )
+void HBluetooth_vSendString(P2VAR(c8_t) A_ptrc8String)
 {
 
 	MUSART_vTransmitString( Bluetooth_UART_ID, A_ptrc8String ) ;
@@ -79,7 +79,7 @@ void HBluetooth_vSendString( c8_t *A_ptrc8String )
 /*******************************************************************************************************************/
 /******************************************************************************************************************/
 
-void HBluetooth_vReceiveString( c8_t A_c8YourString[] )
+void HBluetooth_vReceiveString(P2VAR(c8_t) A_c8YourString)
 {
 
 	MUSART_vRecieveString( Bluetooth_UART_ID, A_c8YourString ) ;
@@ -89,7 +89,7 @@ void HBluetooth_vReceiveString( c8_t A_c8YourString[] )
 /*******************************************************************************************************************/
 /******************************************************************************************************************/
 
-u8_t HBluetooth_u8CompStrings( c8_t *String1, c8_t *String2 )
+FUNC(u8_t) HBluetooth_u8CompStrings(P2VAR(c8_t) String1, P2VAR(c8_t) String2)
 {
 
 	u8_t L_u8Result = INITIAL_ZERO ;
@@ -123,7 +123,18 @@ void HBluetooth_vDisable( void )
 /*******************************************************************************************************************/
 /******************************************************************************************************************/
 
+void HBluetooth_ExchangeString(P2VAR(c8_t) L_strMessagePlaceholder, P2VAR(c8_t) L_strInfo)
+{
+	HBluetooth_vSendString(L_strMessagePlaceholder);
+	HBluetooth_vReceiveString(L_strInfo);
+}
 
-
-
-
+/*******************************************************************************************************************/
+/******************************************************************************************************************/
+FUNC(u8_t) HBluetooth_ExchangeByte(P2VAR(c8_t) L_strMessagePlaceholder)
+{
+	HBluetooth_vSendString(L_strMessagePlaceholder);
+	return HBluetooth_u8ReceiveByte();
+}
+/*******************************************************************************************************************/
+/******************************************************************************************************************/
