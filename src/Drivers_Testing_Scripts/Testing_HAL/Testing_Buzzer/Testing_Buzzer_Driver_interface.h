da7c10dfc4068ced54bfb12f60b1c4d86fe0ce12
Mohamed Alaa <abo.3ala2.a338@gmail.com>
2023-02-07 23:43:47 +0200

    Added Buzzer testing application

---
+++ src/Drivers_Testing_Scripts/Testing_HAL/Testing_Buzzer/Testing_Buzzer_Driver_interface.h
@@ -0,0 +1,13 @@
+/**
+ * @file Testing_Buzzer_Driver_interface.h
+ * @author Mohamed Alaa
+ * @version V1.0
+ * @date 2020-10-20
+ */
+
+#ifndef _TESTING_BUZZER_DRIVER_INTERFACE_H_
+#define _TESTING_BUZZER_DRIVER_INTERFACE_H_
+
+void THBuzzer_vTestBuzzer(void);
+
+#endif //_TESTING_BUZZER_DRIVER_INTERFACE_H_
