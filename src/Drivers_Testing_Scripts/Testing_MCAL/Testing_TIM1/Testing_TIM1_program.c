cca8abe450dbd94c4b3b45aa4b52752317f0a48a
Ali El-bana <alitheboss922@gmail.com>
2023-02-09 18:35:03 +0200

    Modifying some parameters 2

--- src/Drivers_Testing_Scripts/Testing_MCAL/Testing_TIM1/Testing_TIM1_program.c
+++ src/Drivers_Testing_Scripts/Testing_MCAL/Testing_TIM1/Testing_TIM1_program.c
@@ -33,15 +33,9 @@ void TMTIM1_vGeneratePWM( void )
 	MRCC_vInit( ) ;
 
 	MRCC_vEnablePeriphralCLK( RCC_AHB1, AHB1ENR_GPIOAEN ) ;
-	MRCC_vEnablePeriphralCLK( RCC_AHB1, AHB1ENR_GPIOBEN ) ;
-	MRCC_vEnablePeriphralCLK( RCC_APB2, APB2ENR_SYSCFGEN ) ;
+	//MRCC_vEnablePeriphralCLK( RCC_AHB1, AHB1ENR_GPIOBEN ) ;
 
 	MGPIOx_vLockedPins( ) ;
-
-	MNVIC_vEnablePeriphral( TIM1_BRK_TIM9 ) ;
-	MNVIC_vEnablePeriphral( TIM1_UP_TIM10 ) ;
-	MNVIC_vEnablePeriphral( TIM1_TRG_COM_TIM11 ) ;
-	MNVIC_vEnablePeriphral( TIM1_CC ) ;
 
 	MGPIOx_ConfigType T1_CH1 =
 	{
@@ -58,14 +52,10 @@ void TMTIM1_vGeneratePWM( void )
 	MTIM1_vPWMInit( TIM1_CH1 , PWM1, CENTER3,
 					PSC_VALUE, ARR_VALUE, CR_VALUE ) ;
 
-	//MTIM1_vEnableCounter( ) ;
-
-	//MTIM1_vSetCompareReg1Value( 65000 / 2 ) ;
-
 	while( TRUE )
 	{
 
-		for( u16_t L_u16Iteration = 0; L_u16Iteration <= 65530; L_u16Iteration++ )
+		for( u16_t L_u16Iteration = 0; L_u16Iteration <= 65530; L_u16Iteration += 10000 )
 		{
 			MTIM1_vSetCompareReg1Value( L_u16Iteration ) ;
 
